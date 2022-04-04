da65104e64211e7e1d2799aa146178b0b8763cdb
Brahian Araya <brah.araya.a@gmail.com>
2022-04-04 02:29:30 -0400

    cambio de funciones a clases ecmascript

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,21 +1,39 @@
-function MediaPlayer(config) {
-	this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function () {
-	this.media.play();
-};
-
-MediaPlayer.prototype.pause = function () {
-	this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function () {
-	if (this.media.paused) {
-		this.play();
-	} else {
-		this.pause();
+class MediaPlayer {
+	constructor(config) {
+		this.media = config.el;
+		this.plugins = config.plugins || [];
+		this._initPlugins();
 	}
-};
+
+	_initPlugins() {
+		this.plugins.forEach((plugin) => {
+			plugin.run();
+		});
+	}
+
+	play() {
+		this.media.play();
+	}
+
+	pause() {
+		this.media.pause();
+	}
+
+	mute() {
+		this.media.muted = true;
+	}
+
+	unmute() {
+		this.media.muted = false;
+	}
+
+	togglePlay() {
+		if (this.media.paused) {
+			this.play();
+		} else {
+			this.pause();
+		}
+	}
+}
 
 export default MediaPlayer;
